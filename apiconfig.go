3a1d83675e06b6adf00e369a0f7d687d3e04f3da
haidar1337 <haidar.abugurain@gmail.com>
2024-09-03 15:32:33 +0300

    middlewares and routing

---
+++ apiconfig.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) handleNumberOfRequests(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetCounter(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
-	mux.HandleFunc("/healthz", handleReadiness)
+	cfg := apiConfig{
+		fileserverHits: 0,
+	}
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir("./")))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", cfg.handleNumberOfRequests)
+	mux.HandleFunc("/reset", cfg.resetCounter)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -19,7 +24,7 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func handleReadiness(w http.ResponseWriter, req *http.Request) {
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK) // 200
 	w.Write([]byte("OK"))
