14b7c1bacee2bb1ba70c81f58ae6ea268cb483ff
haidar1337 <haidar.abugurain@gmail.com>
2024-09-05 20:14:50 +0300

    ch 4

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -15,6 +17,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", cfg.handleNumberOfRequests)
 	mux.HandleFunc("GET /api/reset", cfg.resetCounter)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -28,4 +31,62 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK) // 200
 	w.Write([]byte("OK"))
+}
+
+func validateChirp(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, "No request body")
+		return
+	}
+	
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	type returnVal struct {
+		Cleaned_body string `json:"cleaned_body"`
+	}
+	const replacement = "****"
+	out := returnVal{
+		Cleaned_body: params.Body,
+	}
+	splitted := strings.Split(params.Body, " ")
+	for i := 0; i < len(splitted); i++ {
+		word := splitted[i]
+		lowered := strings.ToLower(word)
+		if lowered == "kerfuffle" || lowered == "fornax" || lowered == "sharbert" {
+			out.Cleaned_body = strings.Replace(out.Cleaned_body, word, replacement, -1)
+		}
+	}
+	
+	respondWithJSON(w, 200, out)
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, msg string) {
+	type errorVal struct{
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, statusCode, errorVal{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {
+	w.Header().Set("content-type", "application/json")
+	data, err := json.Marshal(&payload)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	w.Write(data)
 }
